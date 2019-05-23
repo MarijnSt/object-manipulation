122d3ed37c76133a2e7034598690c6b4ce15d65e
MarijnSt <marijn.stammeleer@gmail.com>
2019-05-23 10:58:43 +0200

    created arrays and object

--- src/home.php
+++ src/home.php
@@ -5,20 +5,30 @@
   </head>
   <body>
     <?php
-      class Fruit {
-        public $weight = 0;
+      $arr = ["dit", "is", "een", "array"];
+      $assArr = ["naam" => "Marijn", "opleiding" => "becode"];
+      class Voorstelling {
+        //properties
+        public $naam;
+        public $leeftijd;
+        public $woonplaats;
 
-        public function __construct($gewicht){
-          $this->weight = $gewicht;
+        //constructor
+        public function __construct($name, $age, $residence) {
+          $this->naam = $name;
+          $this->leeftijd = $age;
+          $this->woonplaats = $residence;
         }
 
-        public function getWeight(){
-          return 'This fruit weighs ' . $this->weight . 'grams.';
+        //printfunctie
+        public function printerke() {
+          return "Mijn naam is " . $this->naam . " (" . $this->leeftijd . " jaar) en ik woon in " . $this->woonplaats . ".";
         }
       }
 
-      $fruit = new Fruit(250);
-      echo($fruit->getWeight());
+      $ikke = new Voorstelling("Marijn", 26, "Bambrugge");
+      var_dump($ikke);
+      echo ($ikke->printerke());
     ?>
   </body>
 </html>
